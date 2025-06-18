e2e70b4686a263d25b1e66810dc84d5d9284515e
gahtori123 <ayushgahtori76@gmail.com>
2025-06-18 11:27:41 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(`mongodb://127.0.0.1:27017
-/minor`);
+    const conn = await mongoose.connect(`mongodb+srv://gahtori0001:yypnMzeKabbMwK2n@cluster0.ps5mcym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
     `MongoDB Connected: ${conn.connection.host}`;
   } catch (err) {
     console.error(`Error connecting to MongoDB: ${err.message}`);
